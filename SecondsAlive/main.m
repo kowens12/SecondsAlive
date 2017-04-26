0a85f79755a32e30c984a356cad021c4707e41e0
Kate <kowens711@gmail.com>
2017-04-26 09:23:04 -0400

    initial commit

---
+++ SecondsAlive/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SecondsAlive
+//
+//  Created by Kate Owens  on 4/26/17.
+//  Copyright © 2017 Me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
